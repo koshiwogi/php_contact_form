f40eee4d7a02461c1aab2b91b745327dff55cd02
柏木孝洋 <kashiwagitakahiro@kashiwagitakahironoMacBook-Air.local>
2019-07-25 10:37:25 +0800

    お問い合わせ情報

--- check.php
+++ check.php
@@ -1,12 +1,25 @@
 <?php
-    $nickname = $_POST['nickname'];
-    echo $nickname;
-
-    $email = $_POST['email'];
-    echo $email;
-    echo '<br>';
-    $content = $_POST['content'];
-    echo $content;
+     $nickname = $_POST['nickname'];
+     $email = $_POST['email'];
+     $content = $_POST['content'];
+ 
+     if ($nickname == '') {
+         $nickname_result = 'ニックネームが入力されていません。';
+     } else {
+         $nickname_result = 'ようこそ、' . $nickname .'様';
+     }
+     
+     if ($email == '') {
+         $email_result = 'メールアドレスが入力されていません。';
+     } else {
+         $email_result = 'メールアドレス：' . $email;
+     }
+ 
+     if ($content == '') {
+         $content_result =  'お問い合わせ内容が入力されていません。';
+     } else {
+         $content_result = 'お問い合わせ内容：' . $content;
+     }
 ?>
 
 
@@ -18,5 +31,20 @@
 </head>
 <body>
     <h1>入力内容確認</h1>
+    <p><?php echo $nickname_result; ?></p>
+    <p><?php echo $email_result; ?></p>
+    <p><?php echo $content_result; ?></p>
+  <form method="POST" action="thanks.php">
+        <input type="hidden" name="nickname" value="<?php echo $nickname; ?>">
+        <input type="hidden" name="email" value="<?php echo $email; ?>">
+        <input type="hidden" name="content" value="<?php echo $content; ?>">
+        <input type="button" value="戻る" onclick="history.back()">
+        <?php if ($nickname != '' && $email != '' && $content != ''): ?>
+            <input type="submit" value="OK">
+        <?php endif; ?>
+
+</form>
+
+
 </body>
 </html>
