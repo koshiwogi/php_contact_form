f4c2de99663d56f3cefea90bce7f0ff02c700030
柏木孝洋 <kashiwagitakahiro@kashiwagitakahironoMacBook-Air.local>
2019-07-26 10:56:09 +0800

    php

--- thanks.php
+++ thanks.php
@@ -1,11 +1,19 @@
 <?php
+    require_once('function.php');
+    require_once('dbconnect.php'); // 追加
+
   if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     header('Location: index.html');
 }
     $nickname = $_POST['nickname'];
     $email = $_POST['email'];
     $content = $_POST['content'];
+    //SQl分の準備
+    $stmt = $dbh->prepare('INSERT INTO surveys (nickname, email, content) VALUES (?, ?, ?)');
+    $stmt->execute([$nickname, $email, $content]);//?を変数に置き換えてSQLを実行
+
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
